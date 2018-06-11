aff9cacce79cac82a09de6442b0d20cc4347394f
graycalx2@gmail.com <graycalx2@gmail.com>
2018-06-11 18:06:11 +0300

    i3 config

--- bar.py
+++ bar.py
@@ -1,4 +1,6 @@
 from i3pystatus import Status
+from i3pystatus import get_module
+
 
 status = Status()
 
@@ -7,30 +9,35 @@ terminal = "alacritty"
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
-status.register("text",
-        text="",
-        on_rightclick = "%s -e htop" %(terminal),
-        )
+
+@get_module
+def show_date(self):
+    self.output['format'] = '%a %-d %b'
+
+status.register("clock",
+        format="%H:%M",
+        on_leftclick=show_date)
+
+
 
 status.register("battery",
-    format="{status} {remaining:%E%hh:%Mm}",
-    alert=True,
-    alert_percentage=5,
-    status={
-        "DIS":  "Discharging",
-        "CHR":  "Charging",
-        "FULL": "100%",
+        format='{percentage: .0f}% {remaining:%E%hh:%Mm} {status}',
+        alert=True,
+        alert_percentage=5,
+        status={
+        "DIS":  "",
+        "CHR":  "",
+        "FULL": "",
     })
-
-status.register("clock",
-        format="%H:%M",)
-
-status.register("clock",
-        format="%a  %-d %b ",
-        )
 
 status.register("text",
         text="",
         on_leftclick = "pavucontrol")
+
+status.register("network",
+        interface="wlp3s0",
+        hints={"markup": "pango"},
+        on_leftclick="ip addr show dev {interface} | xmessage -file -" 
+    )
 
 status.run()
